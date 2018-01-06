9f3c1ad7002f3b45b02c772f9fc23df1fbedde18
comp <igorshinal@gmail.com>
2018-01-06 15:31:27 +0200

    Comments for developers

---
+++ README.md
@@ -0,0 +1,3 @@
+1. npm init -y
+2. nmp i -D webpack
+3. Create webpack config and run npm run build
--- src/component.js
+++ src/component.js
@@ -1,3 +1,5 @@
+/*Export module with default content for this module*/
+
 export default (text = 'Hello world') => {
     const element = document.createElement('div')
     element.innerHTML = '<p>' + text + '</p>'
