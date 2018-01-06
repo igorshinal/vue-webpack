e1321aa9a9302f57db917132345e71c7f78965a6
comp <igorshinal@gmail.com>
2018-01-06 16:44:32 +0200

    Connect html template plugin in webpack

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 1. npm init -y
 2. nmp i -D webpack
-3. Create webpack config and run npm run build
+3. create webpack config and run npm run build
+4. npm i -D html-webpack-plugin and connect him in webpack config ( he generate html teplate by parametrs )
+5. create sript build.sh for automaticly build
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     /*Точка входа откуда мы будем все собирать*/
@@ -8,6 +9,21 @@ module.exports = {
     /*Точка выхода откуда мы будем все собирать*/
     output: {
         path: path.join(__dirname, 'dist'),
-        filename: 'build.js'
-    }
+        filename: 'js/[name].js'
+    },
+    /*Создаем экземпляр плагина и задаем параметры*/
+    plugins: [
+        new HtmlWebpackPlugin({
+            /*Вставляет заголовок в собранный шаблон*/
+            title: 'Vue webpack template',
+            /*Как будет называться собранный файл*/
+            filename: 'test.html',
+            /*куда нам нужно вставлять скриптовіе теги head false true body*/
+            inject: true,
+            /*Добавляет хэш к урлу собранного скрипта*/
+            hash: true,
+            /*Для генерации собранногго шаблона мы будем использовать index.html*/
+            template: 'index.html'
+        })
+    ]
 }
