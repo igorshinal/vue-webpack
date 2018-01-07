0b66a3b2e2ff350b3abdca847a2e573aeb0ab122
igorshinal <igorshinal@gmail.com>
2018-01-07 23:28:18 +0200

    fix bug in webpack config file with run build dev

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,14 @@ const path = require('path')
 const webpack = require('webpack')
 const merge = require('webpack-merge')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const ProgressBarWebpackPlugin = require('progress-bar-webpack-plugin')
+const ProgressBarPlugin = require('progress-bar-webpack-plugin')
 const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
-const CompressionWebpackPlugin = require('compression-webpack-plugin')
+const CompressionPlugin = require('compression-webpack-plugin')
 
-/*настройки которые касаются обоих сред разработки*/
 const baseConfig = {
-    /*Точка входа откуда мы будем все собирать*/
     entry: {
         app: path.join(__dirname, 'src')
     },
-    /*Точка выхода откуда мы будем все собирать*/
     output: {
         path: path.join(__dirname, 'dist'),
         filename: 'js/[name].js'
@@ -22,46 +19,47 @@ const baseConfig = {
         modules: true,
         reasons: true,
         errorDetails: true
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Vue-webpack template step by step',
+            filename: 'index.html',
+            inject: true,
+            hash: true,
+            template: 'index.html',
+            minify: {
+                removeComments: true,
+                collapseWhitespace: true,
+                removeAttributeQuotes: true
+            }
+        })
+    ]
 }
-
 const productionConfig = () => {
     const config = {
         plugins: [
-            new ProgressBarWebpackPlugin(),
-            /* Работате с переменными окружения */
+            new ProgressBarPlugin(),
             new webpack.DefinePlugin({
                 'process.env': {
                     NODE_ENV: JSON.stringify('production')
                 }
             }),
-            /*Минимизирует айди которые сборщик использует для подгрузки*/
             new webpack.optimize.OccurrenceOrderPlugin(),
-
-            new CompressionWebpackPlugin({
-                /*как будут называться сжатые файлы*/
-                asset:'[path].gz[query]',
-                /*алгоритм сжатия*/
+            new CompressionPlugin({
+                asset: '[path].gz[query]',
                 algorithm: 'gzip',
-                /*расширения файлов которые будут сжаты*/
                 test: /\.(js|css|html|svg)$/,
-                /*сжимать файлы только те которые весят больше 10240кбт*/
-                threshold: 1,
-                /*коэфициент сжатия*/
-                minRatio: 0.9,
-                /*удаляет оригинальніе файлі не сжатіе в сборке*/
-                // deleteOriginalAssets: true
-
+                /* deleteOriginalAssets: true, */
+                threshold: 0,
+                minRatio: 0.9
             })
         ]
     }
     return merge(baseConfig, config)
 }
-
 const developmentConfig = () => {
     const config = {
         devServer: {
-            /*история для работы с пользовательскими путями*/
             historyApiFallback: true,
             stats: 'errors-only',
             watchOptions: {
@@ -71,60 +69,35 @@ const developmentConfig = () => {
             host: process.env.HOST,
             port: process.env.PORT
         },
-        /*Создаем экземпляр плагина и задаем параметры*/
         plugins: [
-            new HtmlWebpackPlugin({
-                /*Вставляет заголовок в собранный шаблон*/
-                title: 'Vue webpack template',
-                /*Как будет называться собранный файл*/
-                filename: 'index.html',
-                /*куда нам нужно вставлять скриптовые теги head false true body*/
-                inject: true,
-                /*Добавляет хэш к урлу собранного скрипта*/
-                hash: true,
-                /*Для генерации собранногго шаблона мы будем использовать index.html*/
-                template: 'index.html',
-                minify: {
-                    /*Убирает комментарии*/
-                    removeComments: true,
-                    /*убирает пробелы*/
-                    collapseWhitespace: true,
-                    /*убирает кавычки*/
-                    removeAttributeQuotes: true
-                }
-            }),
-            /*webpack игорирутет эти файлы и не вотчит*/
-
             new webpack.WatchIgnorePlugin([
                 path.join(__dirname, 'node_modules')
             ]),
 
             new FriendlyErrorsPlugin({
                 compilationSuccessInfo: {
-                    messages: ['You application is running here http://localhost:8080'],
-                    notes: ['Some additional notes to be displayed upon successful compilation']
+                    messages: ['You appication is running here http://localhost:8080', 'You appication is running here http://localhost:8080'],
+                    notes: ['Some additionsl notes to be displayed unpon successful compilatioin']
+                },
+                onErrors: function (severity, errors) {
+                    /*         console.log('severity = ', severity)
+                            console.log('errors = ', errors) */
                 },
                 clearConsole: true
             }),
-            /*Подавляет ошибки и собирает информацию сборки*/
             new webpack.NoEmitOnErrorsPlugin(),
-
-            /* Работате с переменными окружения */
             new webpack.DefinePlugin({
                 'process.env': {
                     NODE_ENV: JSON.stringify('development')
                 }
             })
         ]
-
     }
     return merge(baseConfig, config)
-
 }
 
 module.exports = (env) => {
-console.log('env=', env)
+    console.log('env = ', env)
     if (env === 'production') return productionConfig()
-    return developmentConfig
-
+    return developmentConfig()
 }
